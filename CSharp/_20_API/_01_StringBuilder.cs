cb0ad52444a0c40713d2d8f6175bc634821dd7f6
Jose Santos <airamez@gmail.com>
2025-02-10 20:21:53 -0800

    Aula 243: API - StringBuilder

---
+++ CSharp/_20_API/_01_StringBuilder.cs
@@ -0,0 +1,55 @@
+// https://learn.microsoft.com/en-us/dotnet/api/system.text.stringbuilder?view=net-9.0
+
+using System;
+using System.Diagnostics;
+using System.Text;
+
+namespace DotNetAPI;
+
+public class StringBuilderApp
+{
+  public static void Main(string[] args)
+  {
+    Stopwatch stopwatch = new Stopwatch();
+
+    const int LENGTH = 200_000;
+    Console.WriteLine($"Length = {LENGTH:N0}");
+
+    //DemoString(stopwatch, LENGTH);
+
+    DemoStringBuilder(stopwatch, LENGTH);
+  }
+
+  private static void DemoString(Stopwatch stopwatch, int LENGTH)
+  {
+    stopwatch.Start();
+    long memoryBefore = GC.GetTotalMemory(true);
+    Console.WriteLine($"Memory before: {memoryBefore / (1024 * 1024):N0} MB");
+
+    string myString = "";
+    for (int i = 0; i < LENGTH; i++)
+    {
+      myString += $"{i} ";
+    }
+
+    stopwatch.Stop();
+    Console.WriteLine($"Elapsed Time = {stopwatch.Elapsed}");
+    Console.WriteLine($"GC memory: {GC.GetTotalMemory(true) / (1024 * 1024):N0} MB");
+  }
+
+  private static void DemoStringBuilder(Stopwatch stopwatch, int LENGTH)
+  {
+    stopwatch.Start();
+    long memoryBefore = GC.GetTotalMemory(true);
+    Console.WriteLine($"Memory before: {memoryBefore / (1024 * 1024):N0} MB");
+
+    var sb = new StringBuilder();
+    for (int i = 0; i < LENGTH; i++)
+    {
+      sb.Append($"{i} ");
+    }
+    stopwatch.Stop();
+    Console.WriteLine($"Elapsed Time = {stopwatch.Elapsed}");
+    Console.WriteLine($"GC memory: {GC.GetTotalMemory(true) / (1024 * 1024):N0} MB");
+  }
+}
