eb0b8ee17575b80fda0cf5fd9ab037093ee6a991
Jose Santos <airamez@gmail.com>
2025-03-08 13:18:44 -0800

    FInal review for PiHunter

--- CSharp/_21_Advanced/_13_AsyncPiHunter.cs
+++ CSharp/_21_Advanced/_13_AsyncPiHunter.cs
@@ -18,19 +18,19 @@ public class PiHunterApp
 
 public class PiHunterGame
 {
-  public const int MAP_WIDTH = 40; // Map Width
-  public const int MAP_HEIGHT = 20; // Map Height
+  public const int MAP_WIDTH = 20; // Map Width
+  public const int MAP_HEIGHT = 10; // Map Height
   public const string FLOOR = "░";
   public const string WALL = "▓";
   public const string PI = "π";
   public const string HUNTER = "☻";
   public const string MONSTER = "∞";
   static public bool IsGameOver { private set; get; }
-  static public int PlayerCurrentCol { private set; get; }
-  static public int PlayerCurrentRow { private set; get; }
+  static public int PlayerCol { private set; get; }
+  static public int PlayerRow { private set; get; }
 
-  private int collectedCounter = 0;
-  private int totalToCollectCounter = 0;
+  private int CollectedCounter { set; get; }
+  private int TotalToCollectCounter { set; get; }
   private string[,] map; // 2D array to represent the map
   private readonly object consoleLock = new object(); // Lock object for console synchronization
   private Stopwatch timer = new Stopwatch();
@@ -42,11 +42,13 @@ public class PiHunterGame
     random = new Random();
     Console.Clear();
     Console.OutputEncoding = Encoding.UTF8;
-    Console.SetBufferSize(Math.Max(MAP_WIDTH, Console.BufferWidth), Math.Max(MAP_HEIGHT + 5, Console.BufferHeight));
+    Console.SetBufferSize(Math.Max(MAP_WIDTH, Console.BufferWidth), Math.Max(MAP_HEIGHT + 5, Console.BufferHeight)); // Only works on Windows
     Console.CursorVisible = false; // Hide the cursor for better presentation
+    CollectedCounter = 0;
+    TotalToCollectCounter = 0;
     InitializeMap();
     DrawMap();
-    SpawnMonsters(4); // Spawn three monsters
+    SpawnMonsters(1); // Spawn three monsters
     timer.Start();
     Task.Run(() => UpdateStatus());
     foreach (var monster in monsters)
@@ -56,10 +58,6 @@ public class PiHunterGame
     IsGameOver = false;
     while (!IsGameOver)
     {
-      if (collectedCounter == totalToCollectCounter)
-      {
-        IsGameOver = true;
-      }
       // Wait for user input
       var key = Console.ReadKey(intercept: true).Key;
       // Exit the program if the user presses 'Q' or Escape
@@ -67,22 +65,21 @@ public class PiHunterGame
       {
         IsGameOver = true;
       }
-      // Calculate the new position of the "man"
-      int newCol = PlayerCurrentCol;
-      int newRow = PlayerCurrentRow;
+      int newCol = PlayerCol;
+      int newRow = PlayerRow;
       switch (key)
       {
         case ConsoleKey.UpArrow:
-          newRow = PlayerCurrentRow > 0 ? PlayerCurrentRow - 1 : PlayerCurrentRow;
+          newRow = PlayerRow > 0 ? PlayerRow - 1 : PlayerRow;
           break;
         case ConsoleKey.DownArrow:
-          newRow = PlayerCurrentRow < MAP_HEIGHT - 1 ? PlayerCurrentRow + 1 : PlayerCurrentRow;
+          newRow = PlayerRow < MAP_HEIGHT - 1 ? PlayerRow + 1 : PlayerRow;
           break;
         case ConsoleKey.LeftArrow:
-          newCol = PlayerCurrentCol > 0 ? PlayerCurrentCol - 1 : PlayerCurrentCol;
+          newCol = PlayerCol > 0 ? PlayerCol - 1 : PlayerCol;
           break;
         case ConsoleKey.RightArrow:
-          newCol = PlayerCurrentCol < MAP_WIDTH - 1 ? PlayerCurrentCol + 1 : PlayerCurrentCol;
+          newCol = PlayerCol < MAP_WIDTH - 1 ? PlayerCol + 1 : PlayerCol;
           break;
         case ConsoleKey.Spacebar:
           UseBomb();
@@ -92,18 +89,23 @@ public class PiHunterGame
       if (map[newRow, newCol] != WALL)
       {
         // Erase the hunter from the current position
-        PrintAt(PlayerCurrentRow, PlayerCurrentCol, FLOOR);
+        PrintAt(PlayerRow, PlayerCol, FLOOR);
         // Update the hunter position
-        PlayerCurrentCol = newCol;
-        PlayerCurrentRow = newRow;
-        // Check if the hunter collects an object
-        if (map[PlayerCurrentRow, PlayerCurrentCol] == PI)
+        PlayerCol = newCol;
+        PlayerRow = newRow;
+        // Check if the hunter collected an PI
+        if (map[PlayerRow, PlayerCol] == PI)
         {
-          collectedCounter++;
-          map[PlayerCurrentRow, PlayerCurrentCol] = FLOOR; // Remove the object from the map
+          CollectedCounter++;
+          map[newRow, newCol] = FLOOR;
         }
         // Redraw the hunter at the new position
-        PrintAt(PlayerCurrentRow, PlayerCurrentCol, HUNTER);
+        PrintAt(PlayerRow, PlayerCol, HUNTER);
+        // Check for Victory
+        if (CollectedCounter == TotalToCollectCounter)
+        {
+          IsGameOver = true;
+        }
       }
     }
 
@@ -111,22 +113,22 @@ public class PiHunterGame
     // Exit message
     Console.WriteLine();
     Console.WriteLine("Thanks for playing! Goodbye.");
-    if (collectedCounter == totalToCollectCounter)
+    if (CollectedCounter == TotalToCollectCounter)
     {
       TimeSpan elapsedTime = timer.Elapsed;
       Console.WriteLine($"CONGRATULATIONS!!! You collected all objects in {elapsedTime:hh\\:mm\\:ss}");
     }
     else
     {
-      Console.WriteLine($"BOOOOOOOOOOOOO!!! You only collected {collectedCounter} objects");
+      Console.WriteLine($"BOOOOOOOOOOOOO!!! You only collected {CollectedCounter} objects");
     }
   }
 
   private void UseBomb()
   {
-    for (int row = PlayerCurrentRow - 1; row <= PlayerCurrentRow + 1; row++)
+    for (int row = PlayerRow - 1; row <= PlayerRow + 1; row++)
     {
-      for (int col = PlayerCurrentCol - 1; col <= PlayerCurrentCol + 1; col++)
+      for (int col = PlayerCol - 1; col <= PlayerCol + 1; col++)
       {
         if (row >= 0 && row < MAP_HEIGHT && col >= 0 && col <= MAP_WIDTH)
         {
@@ -150,30 +152,29 @@ public class PiHunterGame
       {
         // Fill the map with the default character
         map[row, col] = FLOOR;
-
         // Randomly place walls
         if (random.Next(100) < 20) // 20% chance for a wall
         {
           map[row, col] = WALL;
         }
-
-        // Randomly place objects
-        if (random.Next(100) < 10 && map[row, col] != WALL) // 10% chance for an object
+        // Randomly place PIs
+        if (random.Next(100) < 10 &&
+            map[row, col] != WALL) // 10% chance for an PI
         {
           map[row, col] = PI;
-          totalToCollectCounter++;
+          TotalToCollectCounter++;
         }
       }
     }
 
     // Ensure the starting position is clear and set it to the middle
-    PlayerCurrentCol = MAP_WIDTH / 2;
-    PlayerCurrentRow = MAP_HEIGHT / 2;
-    if (map[PlayerCurrentRow, PlayerCurrentCol] == PI)
+    PlayerCol = MAP_WIDTH / 2;
+    PlayerRow = MAP_HEIGHT / 2;
+    if (map[PlayerRow, PlayerCol] == PI)
     {
-      totalToCollectCounter--;
+      TotalToCollectCounter--;
     }
-    map[PlayerCurrentRow, PlayerCurrentCol] = HUNTER;
+    map[PlayerRow, PlayerCol] = HUNTER;
   }
 
   private void SpawnMonsters(int count)
@@ -211,7 +212,7 @@ public class PiHunterGame
   private void PrintStatus()
   {
     TimeSpan elapsedTime = timer.Elapsed;
-    string status = $"Collected Objects: {collectedCounter}/{totalToCollectCounter} | Time Elapsed: {elapsedTime:hh\\:mm\\:ss} [Press 'ESC' or 'Q' to quit]";
+    string status = $"Collected Objects: {CollectedCounter}/{TotalToCollectCounter} | Time Elapsed: {elapsedTime:hh\\:mm\\:ss} [Press 'ESC' or 'Q' to quit]";
     PrintAt(MAP_HEIGHT + 1, 0, status);
   }
 
@@ -265,8 +266,8 @@ public class Monster
           newCol < PiHunterGame.MAP_WIDTH && map[newRow, newCol] != PiHunterGame.PI)
       {
         // If monster and player are in the same or adjacents positions
-        if (Math.Abs(monsterRow - PiHunterGame.PlayerCurrentRow) <= 1 &&
-            Math.Abs(monsterCol - PiHunterGame.PlayerCurrentCol) <= 1)
+        if (Math.Abs(monsterRow - PiHunterGame.PlayerRow) <= 1 &&
+            Math.Abs(monsterCol - PiHunterGame.PlayerCol) <= 1)
         {
           PrintAt(PiHunterGame.MAP_HEIGHT + 2, 0, "GAME OVER: The monster caught you!");
           Environment.Exit(0);
