dde96e3e8337f3a5225a7615f0988517e8b89edc
Jose Santos <airamez@gmail.com>
2025-04-23 17:05:49 -0700

    Refactory projects folders and add a new project for DataAccess content

---
+++ _05_DataAccess/_01_ADO.NET/_01_BasicQuery.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ADO.NET
+{
+    public class BasicQueryApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, ADO.NET World!");
+        }
+    }
+}
