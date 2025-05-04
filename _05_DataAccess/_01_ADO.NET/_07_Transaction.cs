6f80bba056f8f2b41650640c19330a696871e6ba
Jose Santos <airamez@gmail.com>
2025-05-04 09:59:54 -0700

    Add another case of Transfer

--- _05_DataAccess/_01_ADO.NET/_07_Transaction.cs
+++ _05_DataAccess/_01_ADO.NET/_07_Transaction.cs
@@ -12,7 +12,7 @@ public class TransactionApp
     while (true)
     {
       /*
-       * Note: Remember to show how a hacker could create money using this design
+       * Note: Remember to show how a hacker could exploit using acccount IDs that don't exist
        */
       try
       {
@@ -23,10 +23,11 @@ public class TransactionApp
         Console.Write("  To Account ID: ");
         int targetId = int.Parse(Console.ReadLine());
 
-        Console.Write("       Amount: ");
+        Console.Write("         Amount: ");
         decimal amount = decimal.Parse(Console.ReadLine());
 
-        bankService.Transfer(sourceId, targetId, amount);
+        bankService.TransferFull(sourceId, targetId, amount);
+        Console.WriteLine("Transfer completed");
       }
       catch (Exception ex)
       {
@@ -65,7 +66,7 @@ public class BankAccountService
     {
       command.Parameters.AddWithValue("@Amount", amount);
       command.Parameters.AddWithValue("@AccountId", accountId);
-      // This closes the security issue when account id does not exist
+      // This fixes the security issue when account id does not exist
       if (command.ExecuteNonQuery() == 0)
       {
         throw new Exception($"Account ID not found: {accountId}");
@@ -94,6 +95,65 @@ public class BankAccountService
           Credit(targetId, amount, connection, transaction);
           transaction.Commit();
           Console.WriteLine("Transfer completed");
+        }
+        catch (Exception)
+        {
+          transaction.Rollback();
+          throw;
+        }
+      }
+    }
+  }
+
+  public void TransferFull(int sourceId, int targetId, decimal amount)
+  {
+    using (var connection = new SqlConnection(ConnectionString.GetConnectionString()))
+    {
+      connection.Open();
+      using (SqlTransaction transaction = connection.BeginTransaction())
+      {
+        try
+        {
+          // Check Balance
+          string balanceQuery = "SELECT Amount from Account WHERE id = @AccountId";
+          using (SqlCommand balanceCommand = new SqlCommand(balanceQuery, connection, transaction))
+          {
+            balanceCommand.Parameters.AddWithValue("@AccountId", sourceId);
+            var result = balanceCommand.ExecuteScalar();
+            // Note: Explain this
+            if (result == null || result == DBNull.Value)
+            {
+              throw new Exception($"Source Account ID not found: {sourceId}");
+            }
+            decimal balance = (decimal)result;
+            if (amount > balance)
+            {
+              throw new Exception($"Insufficient balance in source account: Id={sourceId}; Balance={balance}; Amount={amount}");
+            }
+          }
+          // Credit
+          string sqlDebit = "UPDATE Account SET amount = amount - @Amount WHERE id = @AccountId";
+          using (SqlCommand debitCommand = new SqlCommand(sqlDebit, connection, transaction))
+          {
+            debitCommand.Parameters.AddWithValue("@Amount", amount);
+            debitCommand.Parameters.AddWithValue("@AccountId", sourceId);
+            if (debitCommand.ExecuteNonQuery() == 0)
+            {
+              throw new Exception($"Source Account ID not found: {sourceId}");
+            }
+          }
+          // Debit
+          string sqlCredit = "UPDATE Account SET amount = amount + @Amount WHERE id = @AccountId";
+          using (SqlCommand creditCommand = new SqlCommand(sqlCredit, connection, transaction))
+          {
+            creditCommand.Parameters.AddWithValue("@Amount", amount);
+            creditCommand.Parameters.AddWithValue("@AccountId", targetId);
+            if (creditCommand.ExecuteNonQuery() == 0)
+            {
+              throw new Exception($"Target Account ID not found: {targetId}");
+            }
+          }
+          transaction.Commit();
         }
         catch (Exception)
         {
